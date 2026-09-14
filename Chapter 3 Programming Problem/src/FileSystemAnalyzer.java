e9c1c39a280753677aa00d45914e1be47a2294a1
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-14 11:47:24 -0500

    Phase 2 file

--- Chapter 3 Programming Problem/src/FileSystemAnalyzer.java
+++ Chapter 3 Programming Problem/src/FileSystemAnalyzer.java
@@ -1,5 +1,6 @@
 public class FileSystemAnalyzer {
 
+    // Phase 1: Count all files
     public static int countFilesRecursive(FileSystemItem item) {
 
         // Base Case
@@ -20,5 +21,58 @@ public class FileSystemAnalyzer {
         }
 
         return 0;
+    }
+
+    // Phase 2: Calculate total storage size
+    public static int calculateTotalSizeRecursive(FileSystemItem item) {
+
+        // Base Case
+        if (item instanceof FileItem) {
+            return item.getSizeInKB();
+        }
+
+        // General Case
+        if (item instanceof Folder) {
+            Folder folder = (Folder) item;
+            int sum = 0;
+
+            for (FileSystemItem child : folder.getItems()) {
+                sum += calculateTotalSizeRecursive(child);
+            }
+
+            return sum;
+        }
+
+        return 0;
+    }
+
+    // Phase 2: Find the largest file
+    public static FileItem findLargestFileRecursive(FileSystemItem item) {
+
+        // Base Case
+        if (item instanceof FileItem) {
+            return (FileItem) item;
+        }
+
+        // General Case
+        if (item instanceof Folder) {
+            Folder folder = (Folder) item;
+            FileItem largest = null;
+
+            for (FileSystemItem child : folder.getItems()) {
+                FileItem childLargest = findLargestFileRecursive(child);
+
+                if (childLargest != null &&
+                        (largest == null ||
+                                childLargest.getSizeInKB() > largest.getSizeInKB())) {
+
+                    largest = childLargest;
+                }
+            }
+
+            return largest;
+        }
+
+        return null;
     }
 }
