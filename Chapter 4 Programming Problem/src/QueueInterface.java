b1287f5b749e4a20ade55e48dbe62d3f361c1e3b
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-22 11:48:26 -0500

    Phase 2 file

--- Chapter 4 Programming Problem/src/QueueInterface.java
+++ Chapter 4 Programming Problem/src/QueueInterface.java
@@ -1,6 +1,7 @@
 public interface QueueInterface<T> {
     boolean isEmpty();
     boolean isFull();
+    int size();
     void enqueue(T element) throws QueueOverflowException;
     T dequeue() throws QueueUnderflowException;
     T getFront() throws QueueUnderflowException;
